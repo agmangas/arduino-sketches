32ba4862755fa0c833b4b749d56e544efb01a074
Andres Garcia Mangas <agmangas@gmail.com>
2018-01-25 15:44:56 +0100

    Add logic to control relay and refactor

--- water-level-lock/water-level-lock.ino
+++ water-level-lock/water-level-lock.ino
@@ -1,59 +1,32 @@
 const byte WATER_SENSOR_PIN = 0;
+const byte RELAY_PIN = 10;
 const int LEVEL_THRESHOLD = 150;
 const int CHANGE_THRESHOLD = 10;
 const unsigned long LOOP_DELAY_MS = 2000;
 
-int levelPrev = 0;
-int levelCurr = NULL;
-int levelChanged = NULL;
 char printBuffer[128];
-bool currentStatus = false;
+bool isLevelOverThreshold = false;
 
-void updateWaterLevelVars() {
-  int level = analogRead(WATER_SENSOR_PIN);
-
-  levelCurr = level;
-
-  if (((levelPrev >= levelCurr) && ((levelPrev - levelCurr) > CHANGE_THRESHOLD)) ||
-      ((levelPrev < levelCurr) && ((levelCurr - levelPrev) > CHANGE_THRESHOLD))) {
-    levelChanged = levelCurr;
-    levelPrev = levelCurr;
-  }
+int readWaterLevelSensor() {
+  return analogRead(WATER_SENSOR_PIN);
 }
 
-int getWaterLevelIfChanged() {
-  updateWaterLevelVars();
+int detectWaterLevelSignalEdge() {
+  int level = readWaterLevelSensor();
 
-  int ret;
+  sprintf(printBuffer, "Water level: %d\n", level);
+  Serial.print(printBuffer);
 
-  if (levelChanged != NULL) {
-    ret = levelChanged;
-    levelChanged = NULL;
-  } else {
-    ret = NULL;
-  }
-
-  return ret;
-}
-
-int detectLevelStatusEdge() {
-  int level = getWaterLevelIfChanged();
-
-  if (level != NULL) {
-    sprintf(printBuffer, "ADC%d level is %d\n", WATER_SENSOR_PIN, level);
-    Serial.print(printBuffer);
-
-    if (level > LEVEL_THRESHOLD && currentStatus == false) {
-      Serial.println("##### Water level: ON");
-      Serial.flush();
-      currentStatus = true;
-      return 1;
-    } else if (level <= LEVEL_THRESHOLD && currentStatus == true) {
-      Serial.println("##### Water level: OFF");
-      Serial.flush();
-      currentStatus = false;
-      return -1;
-    }
+  if (level > LEVEL_THRESHOLD && isLevelOverThreshold == false) {
+    Serial.println("### Water level: ON");
+    Serial.flush();
+    isLevelOverThreshold = true;
+    return 1;
+  } else if (level <= LEVEL_THRESHOLD && isLevelOverThreshold == true) {
+    Serial.println("### Water level: OFF");
+    Serial.flush();
+    isLevelOverThreshold = false;
+    return -1;
   }
 
   return 0;
@@ -62,11 +35,21 @@ int detectLevelStatusEdge() {
 void setup() {
   Serial.begin(9600);
 
-  Serial.println(">>>>> Starting water level program");
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, LOW);
+
+  Serial.println(">>> Starting water level program");
   Serial.flush();
 }
 
 void loop() {
-  detectLevelStatusEdge();
+  int edge = detectWaterLevelSignalEdge();
+
+  if (edge == 1) {
+    digitalWrite(RELAY_PIN, HIGH);
+  } else if (edge == -1) {
+    digitalWrite(RELAY_PIN, LOW);
+  }
+
   delay(LOOP_DELAY_MS);
 }
