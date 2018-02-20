7b81ec7b8d70b88f1f95018e5174708bf525dc02
Andrés García Mangas <agmangas@gmail.com>
2018-02-20 20:10:59 +0100

    Require a sequence of consistent reads to open or close the relay

--- water-level-lock/water-level-lock.ino
+++ water-level-lock/water-level-lock.ino
@@ -1,10 +1,14 @@
 const byte WATER_SENSOR_PIN = 0;
 const byte RELAY_PIN = 10;
-const int LEVEL_THRESHOLD = 200;
-const unsigned long LOOP_DELAY_MS = 2000;
+const int LEVEL_THRESHOLD = 185;
+const unsigned long LOOP_DELAY_MS = 700;
+const unsigned int MIN_SEQ_HI = 4;
+const unsigned int MIN_SEQ_LO = 3;
 
 char printBuffer[128];
 bool isLevelOverThreshold = false;
+unsigned int sequenceReadsHi = 0;
+unsigned int sequenceReadsLo = 0;
 
 int readWaterLevelSensor() {
   return analogRead(WATER_SENSOR_PIN);
@@ -13,15 +17,27 @@ int readWaterLevelSensor() {
 int detectWaterLevelSignalEdge() {
   int level = readWaterLevelSensor();
 
-  sprintf(printBuffer, "Water level: %d\n", level);
+  if (level > LEVEL_THRESHOLD) {
+    sequenceReadsHi += 1;
+    sequenceReadsLo = 0;
+  } else if (level <= LEVEL_THRESHOLD) {
+    sequenceReadsLo += 1;
+    sequenceReadsHi = 0;
+  }
+
+  sprintf(printBuffer, "Level: %d / HI: %d / LO: %d\n", level, sequenceReadsHi, sequenceReadsLo);
   Serial.print(printBuffer);
 
-  if (level > LEVEL_THRESHOLD && isLevelOverThreshold == false) {
+  if (level > LEVEL_THRESHOLD &&
+      isLevelOverThreshold == false &&
+      sequenceReadsHi >= MIN_SEQ_HI) {
     Serial.println("### Water level: ON");
     Serial.flush();
     isLevelOverThreshold = true;
     return 1;
-  } else if (level <= LEVEL_THRESHOLD && isLevelOverThreshold == true) {
+  } else if (level <= LEVEL_THRESHOLD &&
+             isLevelOverThreshold == true &&
+             sequenceReadsLo >= MIN_SEQ_LO) {
     Serial.println("### Water level: OFF");
     Serial.flush();
     isLevelOverThreshold = false;
