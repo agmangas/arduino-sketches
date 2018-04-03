b533db2abd60ca5265cf5901666ddca75cf32f11
Andres Garcia Mangas <agmangas@gmail.com>
2018-04-03 14:24:49 +0200

    Detect vibrations by counting level changes instead of level distributions

--- reanimathor-phase-02/reanimathor-phase-02.ino
+++ reanimathor-phase-02/reanimathor-phase-02.ino
@@ -32,8 +32,9 @@ const int STATE_BUFFER_SIZE = 10;
 // Buffer that will contain the history of vibration states
 CircularBuffer<StateSample, STATE_BUFFER_SIZE> stateBuffer;
 
-// Vibration ratio threshold
-const float VIBRATION_RATIO = 0.7;
+// Minimum ratio of level changes in a sensor samples
+// stream to consider that the sensor is vibrating
+const float VIBRATION_LEVEL_CHANGE_RATIO = 0.1;
 
 // Iteration delay (ms)
 const int LOOP_WAIT_MS = 10;
@@ -64,25 +65,23 @@ byte getSensorBufferState() {
     return STATE_UNKNOWN;
   }
 
-  int counterHi = 0;
-  int counterLo = 0;
+  int levelChanges = 0;
+  int currentLevel = sensorBuffer[0].level;
 
   for (int i = 0; i < sensorBuffer.size(); i++) {
-    if (sensorBuffer[i].level == HIGH) {
-      counterHi += 1;
-    } else {
-      counterLo += 1;
+    if (currentLevel != sensorBuffer[i].level) {
+      levelChanges += 1;
+      currentLevel = sensorBuffer[i].level;
     }
   }
 
   float bufferSize = sensorBuffer.size();
+  float levelChangesRatio = levelChanges / bufferSize;
 
-  if (counterHi / bufferSize >= VIBRATION_RATIO) {
-    return STATE_STABLE;
-  } else if (counterLo / bufferSize >= VIBRATION_RATIO) {
-    return STATE_STABLE;
-  } else {
+  if (levelChangesRatio >= VIBRATION_LEVEL_CHANGE_RATIO) {
     return STATE_VIBRATING;
+  } else {
+    return STATE_STABLE;
   }
 }
 
