ea23342d9bd7bfc47a9ea58f44880c3b90e7a926
Andres Garcia Mangas <agmangas@gmail.com>
2018-05-02 20:37:45 +0200

    Stop capturing button events when all phases have been completed

--- caldera-neuronal/caldera-neuronal.ino
+++ caldera-neuronal/caldera-neuronal.ino
@@ -9,6 +9,11 @@ typedef struct solutionKeyItem {
   bool btnState;
 } SolutionKeyItem;
 
+typedef struct programState {
+  int currPhase;
+  boolean isFinished;
+} ProgramState;
+
 const int TOTAL_BUTTONS = 9;
 const int NUM_PHASES = 3;
 
@@ -63,14 +68,16 @@ SolutionKeyItem solutionKeys[NUM_PHASES][TOTAL_BUTTONS] = {
   }
 };
 
-unsigned long currPhase = 0;
+ProgramState programState = { .currPhase = 0, .isFinished = false };
 
-boolean getBtnLedState(byte btnIndex) {
-  return atmLeds[btnIndex].state() != atmLeds[btnIndex].IDLE;
+boolean getBtnLedState(int btnIndex) {
+  return atmLeds[btnIndex].state() !=
+         atmLeds[btnIndex].IDLE;
 }
 
-boolean isBtnValid(byte btnIndex) {
-  return solutionKeys[currPhase][btnIndex].btnState == getBtnLedState(btnIndex);
+boolean isBtnValid(int btnIndex) {
+  return solutionKeys[programState.currPhase][btnIndex].btnState ==
+         getBtnLedState(btnIndex);
 }
 
 boolean isButtonPatternValid() {
@@ -83,35 +90,62 @@ boolean isButtonPatternValid() {
   return true;
 }
 
-int getNextBtnIndexToPress() {
+boolean isNextBtnToPress(int btnIndex) {
+  int nextIdx = -1;
+
   for (int i = 0; i < TOTAL_BUTTONS; i++) {
-    if (solutionKeys[currPhase][i].btnState == true &&
+    if (solutionKeys[programState.currPhase][i].btnState == true &&
         getBtnLedState(i) == false) {
-      return i;
+      nextIdx = i;
+      break;
     }
   }
 
-  return -1;
+  return btnIndex == nextIdx;
 }
 
-void resetBtnLeds() {
+void turnAllLedsOff() {
   for (int i = 0; i < TOTAL_BUTTONS; i++) {
     atmLeds[i].trigger(atmLeds[i].EVT_OFF);
   }
 }
 
-void onButtonChange(int idx, int v, int up) {
-  int nextIdx = getNextBtnIndexToPress();
+void turnLedOn(int btnIndex) {
+  atmLeds[btnIndex].trigger(atmLeds[btnIndex].EVT_ON);
+}
 
-  if (idx != nextIdx) {
-    resetBtnLeds();
-  } else {
-    atmLeds[idx].trigger(atmLeds[idx].EVT_TOGGLE);
+void blinkLeds() {
+  for (int i = 0; i < TOTAL_BUTTONS; i++) {
+    atmLeds[i].blink(50, 200).trigger(atmLeds[i].EVT_BLINK);
+  }
+}
+
+boolean isLastPhase() {
+  return programState.currPhase == (NUM_PHASES - 1);
+}
+
+void onButtonChange(int idx, int v, int up) {
+  if (programState.isFinished) {
+    return;
   }
 
-  if (isButtonPatternValid()) {
-    currPhase++;
-    resetBtnLeds();
+  if (isNextBtnToPress(idx)) {
+    turnLedOn(idx);
+  } else {
+    turnAllLedsOff();
+  }
+
+  if (!isButtonPatternValid()) {
+    return;
+  }
+
+  if (isLastPhase()) {
+    programState.isFinished = true;
+    turnAllLedsOff();
+    blinkLeds();
+  } else {
+    programState.currPhase++;
+    turnAllLedsOff();
   }
 }
 
