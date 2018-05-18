2836d5796b643ed4e0c232ad753c557fe0b526d6
Andrés García Mangas <agmangas@gmail.com>
2018-05-18 22:23:33 +0200

    Add support for second strip and encoder events

--- caldera-neuronal-phase-02/caldera-neuronal-phase-02.ino
+++ caldera-neuronal-phase-02/caldera-neuronal-phase-02.ino
@@ -10,14 +10,17 @@
 #define NEOPIXEL_NUM 60
 #define NEOPIXEL_PIN 5
 
+#define NEOPIXEL_NUM_ENC 60
+#define NEOPIXEL_PIN_ENC 6
+
 /**
    Rotary encoder pins:
    A and B are the white and green wires
    Red wire: 5V
    Black wire: GND
 */
-#define ENC_PIN_A 6
-#define ENC_PIN_B 7
+#define ENC_PIN_A 8
+#define ENC_PIN_B 9
 
 typedef struct potInfo {
   int potPin;
@@ -25,10 +28,15 @@ typedef struct potInfo {
 
 typedef struct programState {
   bool isEncoderActive;
+  bool isEncoderDebounce;
+  int encoderLevel;
 } ProgramState;
 
+const int ENCODER_TIMER_MS = 5000;
+const int MAX_ENCODER_LEVEL = NEOPIXEL_NUM_ENC;
+
 const int ENC_RANGE_LO = 0;
-const int ENC_RANGE_HI = 50;
+const int ENC_RANGE_HI = 30;
 
 const int POT_RANGE_LO = 0;
 const int POT_RANGE_HI = 10;
@@ -46,22 +54,36 @@ int potSolutionKey[TOTAL_POTS] = { 3, 4, 5 };
 Atm_analog pots[TOTAL_POTS];
 Atm_controller potsController;
 Atm_encoder rotEncoder;
+Atm_timer encoderLevelTimer;
 
 SoftwareSerial ss = SoftwareSerial(SFX_TX, SFX_RX);
 Adafruit_Soundboard sfx = Adafruit_Soundboard(&ss, NULL, SFX_RST);
 
 Adafruit_NeoPixel pixelStrip = Adafruit_NeoPixel(NEOPIXEL_NUM, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixelStripEnc = Adafruit_NeoPixel(NEOPIXEL_NUM_ENC, NEOPIXEL_PIN_ENC, NEO_GRB + NEO_KHZ800);
+
+const unsigned long POTS_STRIP_DELAY_STEP_MS = 100;
+
+const uint32_t POTS_VALID_COLOR = pixelStripEnc.Color(200, 0, 0);
+const uint32_t ENCODER_COLOR = pixelStripEnc.Color(200, 0, 0);
 
 ProgramState programState = {
-  .isEncoderActive = false
+  .isEncoderActive = false,
+  .isEncoderDebounce = false,
+  .encoderLevel = 0
 };
 
-void setStripOff() {
+void setStripsOff() {
   for (int i = 0; i < NEOPIXEL_NUM; i++) {
     pixelStrip.setPixelColor(i, 0, 0, 0);
   }
 
+  for (int i = 0; i < NEOPIXEL_NUM_ENC; i++) {
+    pixelStripEnc.setPixelColor(i, 0, 0, 0);
+  }
+
   pixelStrip.show();
+  pixelStripEnc.show();
 }
 
 void onPotChange(int idx, int v, int up) {
@@ -83,8 +105,21 @@ bool isPotsSolutionValid(int idx) {
   return true;
 }
 
+void activateValidPotsStrip() {
+  for (int i = 0; i < NEOPIXEL_NUM; i++) {
+    pixelStrip.setPixelColor(i, POTS_VALID_COLOR);
+    pixelStrip.show();
+    delay(POTS_STRIP_DELAY_STEP_MS);
+  }
+}
+
 void onPotsSolutionValid(int idx, int v, int up) {
-  Serial.println("Potentiometers: Valid");
+  if (programState.isEncoderActive) {
+    return;
+  }
+
+  Serial.println("Potentiometers: Valid combination");
+  activateValidPotsStrip();
   programState.isEncoderActive = true;
 }
 
@@ -95,6 +130,51 @@ void onRotEncoderChange(int idx, int v, int up) {
   Serial.print(v);
   Serial.println();
   Serial.flush();
+
+  int mean = (ENC_RANGE_LO + ENC_RANGE_HI) / 2;
+  int tolerance = ENC_RANGE_HI * 0.1;
+
+  if (v == ENC_RANGE_LO && !programState.isEncoderDebounce) {
+    Serial.println("Encoder event");
+    programState.isEncoderDebounce = true;
+    increaseEncoderLevel();
+  } else if (v >= (mean - tolerance) && v <= (mean + tolerance)) {
+    programState.isEncoderDebounce = false;
+  }
+
+  updateEncoderStrip();
+}
+
+void updateEncoderStrip() {
+  for (int i = 0; i < NEOPIXEL_NUM_ENC; i++) {
+    pixelStripEnc.setPixelColor(i, 0, 0, 0);
+  }
+
+  for (int i = 0; i < programState.encoderLevel; i++) {
+    pixelStripEnc.setPixelColor(i, ENCODER_COLOR);
+  }
+
+  pixelStripEnc.show();
+}
+
+void increaseEncoderLevel() {
+  if (programState.encoderLevel < MAX_ENCODER_LEVEL) {
+    programState.encoderLevel++;
+    Serial.print("Encoder level: ");
+    Serial.println(programState.encoderLevel);
+  }
+}
+
+void decreaseEncoderLevel(int idx, int v, int up) {
+  if (programState.encoderLevel >= MAX_ENCODER_LEVEL) {
+    return;
+  }
+
+  if (programState.encoderLevel > 0) {
+    programState.encoderLevel--;
+    Serial.print("Encoder level: ");
+    Serial.println(programState.encoderLevel);
+  }
 }
 
 void initMachines() {
@@ -113,6 +193,11 @@ void initMachines() {
   rotEncoder.begin(ENC_PIN_A, ENC_PIN_B)
   .range(ENC_RANGE_LO, ENC_RANGE_HI, true)
   .onChange(onRotEncoderChange);
+
+  encoderLevelTimer
+  .begin(ENCODER_TIMER_MS)
+  .repeat(-1)
+  .onTimer(decreaseEncoderLevel);
 }
 
 void initStrip() {
@@ -120,7 +205,11 @@ void initStrip() {
   pixelStrip.setBrightness(250);
   pixelStrip.show();
 
-  setStripOff();
+  pixelStripEnc.begin();
+  pixelStripEnc.setBrightness(250);
+  pixelStripEnc.show();
+
+  setStripsOff();
 }
 
 void initSfx() {
