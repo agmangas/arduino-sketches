4b5db1cc556e81ce41123f49f33e1f1ab3b71deb
Andres Garcia Mangas <agmangas@gmail.com>
2018-06-19 20:15:17 +0200

    Remove second strip and refactor effects

--- storm-catcher/storm-catcher.ino
+++ storm-catcher/storm-catcher.ino
@@ -12,17 +12,19 @@ typedef struct windowState {
   byte effect;
 } WindowState;
 
-const uint16_t NEOPIX_NUM_01 = 150;
+const byte PIN_AUDIO_TRACK_THUNDER_01 = 9;
+const byte PIN_AUDIO_TRACK_THUNDER_02 = 10;
+
+const uint16_t NEOPIX_NUM_01 = 300;
 const uint8_t NEOPIX_PIN_01 = 12;
 
-const uint16_t NEOPIX_NUM_02 = 150;
-const uint8_t NEOPIX_PIN_02 = 13;
+const uint32_t COLOR_ELECTRIC_BLUE = Adafruit_NeoPixel::Color(44, 117, 255);
+const uint32_t COLOR_STORM_BLUE_LIGHT = Adafruit_NeoPixel::Color(66, 63, 97);
+const uint32_t COLOR_STORM_BLUE_DARK = Adafruit_NeoPixel::Color(38, 37, 66);
+const uint32_t COLOR_STORM_GREY = Adafruit_NeoPixel::Color(178, 209, 200);
 
 NeoStrip strip01 = NeoStrip(NEOPIX_NUM_01, NEOPIX_PIN_01, NEO_GRB + NEO_KHZ800);
-NeoStrip strip02 = NeoStrip(NEOPIX_NUM_02, NEOPIX_PIN_02, NEO_GRB + NEO_KHZ800);
-
-NeoWindow window01 = NeoWindow(&strip01, 40, NEOPIX_NUM_01);
-NeoWindow window02 = NeoWindow(&strip02, 0, NEOPIX_NUM_02);
+NeoWindow window01 = NeoWindow(&strip01, 0, NEOPIX_NUM_01);
 
 WindowState win01State = {
   .counter = 0,
@@ -30,19 +32,17 @@ WindowState win01State = {
   .effect = ID_EFFECT_NONE
 };
 
-WindowState win02State = {
-  .counter = 0,
-  .nextStepAllowed = true,
-  .effect = ID_EFFECT_NONE
-};
-
-const int LEN_EFFECTS_QUEUE = 2;
-int effectsCounter = 0;
+const int LEN_EFFECTS_QUEUE = 5;
 
 byte effectsQueue[LEN_EFFECTS_QUEUE] = {
   ID_EFFECT_CALM,
+  ID_EFFECT_STORM,
+  ID_EFFECT_CALM,
+  ID_EFFECT_CALM,
   ID_EFFECT_STORM
 };
+
+int effectsCounter = 0;
 
 void initStrip(NeoStrip &strip) {
   strip.begin();
@@ -53,7 +53,6 @@ void initStrip(NeoStrip &strip) {
 
 void initStrips() {
   initStrip(strip01);
-  initStrip(strip02);
 }
 
 void resetWindow(WindowState &winState, NeoWindow &window) {
@@ -63,7 +62,7 @@ void resetWindow(WindowState &winState, NeoWindow &window) {
   window.setNoEfx();
 }
 
-void runCalmEffect(WindowState &winState, NeoWindow &window, NeoStrip &strip) {
+void runCalmEffect(WindowState &winState, NeoWindow &window) {
   if (winState.effect != ID_EFFECT_CALM) {
     return;
   }
@@ -80,9 +79,9 @@ void runCalmEffect(WindowState &winState, NeoWindow &window, NeoStrip &strip) {
     case 0:
       Serial.println("Calm::0");
       window.setFadeEfx(
-        strip.Color(0, 40, 171),
-        strip.Color(51, 65, 106),
-        20, window.fadeTypeCycle, random(1, 5));
+        COLOR_STORM_BLUE_LIGHT,
+        COLOR_STORM_GREY,
+        20, window.fadeTypeCycle, random(2, 5));
       break;
     default:
       Serial.println("Calm::Reset");
@@ -90,7 +89,7 @@ void runCalmEffect(WindowState &winState, NeoWindow &window, NeoStrip &strip) {
   }
 }
 
-void runStormEffect(WindowState &winState, NeoWindow &window, NeoStrip &strip) {
+void runStormEffect(WindowState &winState, NeoWindow &window) {
   if (winState.effect != ID_EFFECT_STORM) {
     return;
   }
@@ -107,28 +106,30 @@ void runStormEffect(WindowState &winState, NeoWindow &window, NeoStrip &strip) {
     case 0:
       Serial.println("Storm::0");
       window.setFadeEfx(
-        strip.Color(0, 40, 171),
-        strip.Color(0, 71, 171),
-        0, window.fadeTypeJumpBack, random(1, 10));
+        COLOR_STORM_BLUE_LIGHT,
+        COLOR_STORM_GREY,
+        0, window.fadeTypeJumpBack, random(1, 2));
       break;
     case 1:
       Serial.println("Storm::1");
       window.setBlinkEfx(
-        strip.Color(0, 71, 171), 20, random(1, 30));
+        COLOR_STORM_BLUE_DARK,
+        20, random(1, 30));
       break;
     case 2:
       Serial.println("Storm::2");
+      playRandomThunder();
       window.setFadeEfx(
-        strip.Color(125, 249, 255),
-        strip.Color(0, 71, 171),
+        COLOR_STORM_BLUE_DARK,
+        COLOR_STORM_BLUE_LIGHT,
         0, window.fadeTypeJumpBack, random(1, 2));
       break;
     case 3:
       Serial.println("Storm::3");
       window.setMultiSparkleEfx(
-        strip.Color(230, 230, 230),
+        COLOR_ELECTRIC_BLUE,
         random(5, 20), random(5, 20),
-        random(40, 60), random(10, 100));
+        random(20, 60), random(100, 150));
       break;
     default:
       Serial.println("Storm::Reset");
@@ -150,8 +151,8 @@ void runStrips() {
   NeoWindow::updateTime();
 
   setNextEffect(win01State);
-  runCalmEffect(win01State, window01, strip01);
-  runStormEffect(win01State, window01, strip01);
+  runCalmEffect(win01State, window01);
+  runStormEffect(win01State, window01);
   updateAndShow(win01State, window01, strip01);
 }
 
@@ -170,14 +171,43 @@ void setNextEffect(WindowState &winState) {
   winState.effect = effect;
 }
 
+void playTrack(byte trackPin) {
+  Serial.print("Playing track: ");
+  Serial.println(trackPin);
+
+  digitalWrite(trackPin, LOW);
+  delay(500);
+  digitalWrite(trackPin, HIGH);
+}
+
+void playRandomThunder() {
+  int randVal = random(0, 100);
+
+  if (randVal > 50) {
+    playTrack(PIN_AUDIO_TRACK_THUNDER_01);
+  } else {
+    playTrack(PIN_AUDIO_TRACK_THUNDER_02);
+  }
+}
+
+void initAudioPins() {
+  pinMode(PIN_AUDIO_TRACK_THUNDER_01, OUTPUT);
+  digitalWrite(PIN_AUDIO_TRACK_THUNDER_01, HIGH);
+
+  pinMode(PIN_AUDIO_TRACK_THUNDER_02, OUTPUT);
+  digitalWrite(PIN_AUDIO_TRACK_THUNDER_02, HIGH);
+}
+
 void setup() {
+  initAudioPins();
+
   randomSeed(analogRead(0));
 
   Serial.begin(9600);
 
   initStrips();
 
-  Serial.println(">> Starting Storm Catcher program");
+  Serial.println(">> Starting Storm Effects program");
 }
 
 void loop() {
