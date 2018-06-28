65b231fc4f37a14c32d3a3441acfb97a7cca0586
Andres Garcia Mangas <agmangas@gmail.com>
2018-06-28 19:16:47 +0200

    Reduce brightness and update effects

--- storm-effects/storm-effects.ino
+++ storm-effects/storm-effects.ino
@@ -18,7 +18,6 @@ const uint8_t NEOPIX_PIN_01 = 12;
 const uint32_t COLOR_ELECTRIC_BLUE = Adafruit_NeoPixel::Color(44, 117, 255);
 const uint32_t COLOR_STORM_BLUE_LIGHT = Adafruit_NeoPixel::Color(66, 63, 97);
 const uint32_t COLOR_STORM_BLUE_DARK = Adafruit_NeoPixel::Color(38, 37, 66);
-const uint32_t COLOR_STORM_GREY = Adafruit_NeoPixel::Color(178, 209, 200);
 
 NeoStrip strip01 = NeoStrip(NEOPIX_NUM_01, NEOPIX_PIN_01, NEO_GRB + NEO_KHZ800);
 NeoWindow window01 = NeoWindow(&strip01, 0, NEOPIX_NUM_01);
@@ -33,9 +32,6 @@ const int LEN_EFFECTS_QUEUE = 5;
 
 byte effectsQueue[LEN_EFFECTS_QUEUE] = {
   ID_EFFECT_CALM,
-  ID_EFFECT_STORM,
-  ID_EFFECT_CALM,
-  ID_EFFECT_CALM,
   ID_EFFECT_STORM
 };
 
@@ -43,7 +39,7 @@ int effectsCounter = 0;
 
 void initStrip(NeoStrip &strip) {
   strip.begin();
-  strip.setBrightness(255);
+  strip.setBrightness(70);
   strip.clearStrip();
   strip.show();
 }
@@ -76,9 +72,9 @@ void runCalmEffect(WindowState &winState, NeoWindow &window) {
     case 0:
       Serial.println("Calm::0");
       window.setFadeEfx(
+        COLOR_ELECTRIC_BLUE,
         COLOR_STORM_BLUE_LIGHT,
-        COLOR_STORM_GREY,
-        20, window.fadeTypeCycle, random(2, 5));
+        40, window.fadeTypeCycle, random(0, 1));
       break;
     default:
       Serial.println("Calm::Reset");
@@ -104,7 +100,7 @@ void runStormEffect(WindowState &winState, NeoWindow &window) {
       Serial.println("Storm::0");
       window.setFadeEfx(
         COLOR_STORM_BLUE_LIGHT,
-        COLOR_STORM_GREY,
+        COLOR_ELECTRIC_BLUE,
         0, window.fadeTypeJumpBack, random(1, 2));
       break;
     case 1:
@@ -115,17 +111,15 @@ void runStormEffect(WindowState &winState, NeoWindow &window) {
       break;
     case 2:
       Serial.println("Storm::2");
-      window.setFadeEfx(
-        COLOR_STORM_BLUE_DARK,
-        COLOR_STORM_BLUE_LIGHT,
-        0, window.fadeTypeJumpBack, random(1, 2));
-      break;
-    case 3:
-      Serial.println("Storm::3");
       window.setMultiSparkleEfx(
         COLOR_ELECTRIC_BLUE,
         random(5, 20), random(5, 20),
-        random(20, 60), random(100, 150));
+        random(20, 60), random(120, 170));
+      break;
+    case 3:
+      Serial.println("Storm::3");
+      window.setBlinkEfx(
+        COLOR_ELECTRIC_BLUE, 40, 10);
       break;
     default:
       Serial.println("Storm::Reset");
