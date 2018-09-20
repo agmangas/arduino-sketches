6aa5acf28136100078ae1553b754ac0e8e3e938d
Andres Garcia Mangas <agmangas@gmail.com>
2018-09-20 09:49:30 +0200

    Lock relay after a while

--- misc/light-lock/light-lock.ino
+++ misc/light-lock/light-lock.ino
@@ -7,11 +7,13 @@
 typedef struct programState {
   bool isOpen;
   int activeCounter;
+  unsigned long openTimeMillis;
 } ProgramState;
 
 ProgramState progState = {
   .isOpen = false,
-  .activeCounter = 0
+  .activeCounter = 0,
+  .openTimeMillis = 0
 };
 
 /**
@@ -20,13 +22,14 @@ ProgramState progState = {
 
 const int DELAY_LOOP_MS = 50;
 const int MIN_ACTIVE_COUNTER = 20;
+const unsigned long OPEN_INTERVAL_MS = 30000;
 
 /**
    Photoresistor constants.
 */
 
-const int NUM_PHOTORESISTORS = 2;
-const int PIN_PHOTORESISTORS[NUM_PHOTORESISTORS] = {2, 3};
+const int NUM_LDR = 2;
+const int PIN_LDR[NUM_LDR] = {2, 3};
 
 /**
    LED strips.
@@ -34,20 +37,20 @@ const int PIN_PHOTORESISTORS[NUM_PHOTORESISTORS] = {2, 3};
 
 const int DEFAULT_BRIGHTNESS = 100;
 
-const uint16_t NEOPIXEL_NUMS[NUM_PHOTORESISTORS] = {
+const uint16_t NEOPIXEL_NUMS[NUM_LDR] = {
   1, 1
 };
 
-const uint8_t NEOPIXEL_PINS[NUM_PHOTORESISTORS] = {
+const uint8_t NEOPIXEL_PINS[NUM_LDR] = {
   11, 12
 };
 
-const uint32_t COLORS[NUM_PHOTORESISTORS] = {
+const uint32_t COLORS[NUM_LDR] = {
   Adafruit_NeoPixel::Color(255, 0, 255),
   Adafruit_NeoPixel::Color(255, 0, 255)
 };
 
-Adafruit_NeoPixel pixelStrips[NUM_PHOTORESISTORS];
+Adafruit_NeoPixel pixelStrips[NUM_LDR];
 
 /**
    Relay constants.
@@ -60,14 +63,14 @@ const int PIN_RELAY = 6;
 */
 
 void initPhoto() {
-  for (int i = 0; i < NUM_PHOTORESISTORS; i++) {
-    pinMode(PIN_PHOTORESISTORS[i], INPUT);
+  for (int i = 0; i < NUM_LDR; i++) {
+    pinMode(PIN_LDR[i], INPUT);
   }
 }
 
 bool arePhotoActivated() {
-  for (int i = 0; i < NUM_PHOTORESISTORS; i++) {
-    if (digitalRead(PIN_PHOTORESISTORS[i]) == HIGH) {
+  for (int i = 0; i < NUM_LDR; i++) {
+    if (digitalRead(PIN_LDR[i]) == HIGH) {
       return false;
     }
   }
@@ -77,7 +80,8 @@ bool arePhotoActivated() {
 
 void checkPhotoUpdateCounter() {
   if (arePhotoActivated()) {
-    progState.activeCounter++;
+    progState.activeCounter = progState.activeCounter < MIN_ACTIVE_COUNTER ?
+                              progState.activeCounter + 1 : MIN_ACTIVE_COUNTER;
   } else {
     progState.activeCounter = 0;
   }
@@ -88,7 +92,7 @@ void checkPhotoUpdateCounter() {
 */
 
 void initLedStrips() {
-  for (int i = 0; i < NUM_PHOTORESISTORS; i++) {
+  for (int i = 0; i < NUM_LDR; i++) {
     pixelStrips[i] = Adafruit_NeoPixel(NEOPIXEL_NUMS[i], NEOPIXEL_PINS[i], NEO_GRB + NEO_KHZ800);
     pixelStrips[i].begin();
     pixelStrips[i].setBrightness(DEFAULT_BRIGHTNESS);
@@ -97,21 +101,17 @@ void initLedStrips() {
   }
 }
 
-void updateLedStrip(int idx) {
-  pixelStrips[idx].clear();
-
-  if (progState.isOpen) {
-    for (int j = 0; j < NEOPIXEL_NUMS[idx]; j++) {
-      pixelStrips[idx].setPixelColor(j, COLORS[idx]);
-    }
-  }
-
-  pixelStrips[idx].show();
-}
-
 void updateLedStrips() {
-  for (int i = 0; i < NUM_PHOTORESISTORS; i++) {
-    updateLedStrip(i);
+  for (int i = 0; i < NUM_LDR; i++) {
+    pixelStrips[i].clear();
+
+    if (progState.isOpen) {
+      for (int j = 0; j < NEOPIXEL_NUMS[i]; j++) {
+        pixelStrips[i].setPixelColor(j, COLORS[i]);
+      }
+    }
+
+    pixelStrips[i].show();
   }
 }
 
@@ -120,18 +120,12 @@ void updateLedStrips() {
 */
 
 void lockRelay() {
-  if (digitalRead(PIN_RELAY) == HIGH) {
-    Serial.println("Relay:Lock");
-  }
-
+  Serial.println("Relay:Lock");
   digitalWrite(PIN_RELAY, LOW);
 }
 
 void openRelay() {
-  if (digitalRead(PIN_RELAY) == LOW) {
-    Serial.println("Relay:Open");
-  }
-
+  Serial.println("Relay:Open");
   digitalWrite(PIN_RELAY, HIGH);
 }
 
@@ -144,8 +138,41 @@ void checkCounterToOpenRelay() {
   if (progState.activeCounter >= MIN_ACTIVE_COUNTER && !progState.isOpen) {
     Serial.println("## Counter over threshold: Opening lock");
     progState.isOpen = true;
+    progState.openTimeMillis = millis();
     digitalWrite(LED_BUILTIN, HIGH);
     openRelay();
+  }
+}
+
+bool shouldResetRelay() {
+  if (!progState.isOpen) {
+    return false;
+  }
+
+  unsigned long now = millis();
+
+  bool isUndefined = progState.openTimeMillis == 0;
+  bool isOverflow = now < progState.openTimeMillis;
+
+  if (isUndefined || isOverflow) {
+    return true;
+  }
+
+  return (now - progState.openTimeMillis) >= OPEN_INTERVAL_MS;
+}
+
+void resetRelay() {
+  Serial.println("## Resetting relay");
+  progState.isOpen = false;
+  progState.openTimeMillis = 0;
+  progState.activeCounter = 0;
+  digitalWrite(LED_BUILTIN, LOW);
+  lockRelay();
+}
+
+void resetRelayIfTimePassed() {
+  if (shouldResetRelay()) {
+    resetRelay();
   }
 }
 
@@ -169,6 +196,7 @@ void setup() {
 void loop() {
   checkPhotoUpdateCounter();
   checkCounterToOpenRelay();
+  resetRelayIfTimePassed();
   updateLedStrips();
   delay(DELAY_LOOP_MS);
 }
