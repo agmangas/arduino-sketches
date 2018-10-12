91a6511fbad86d665461a43e8214751ad8ff4e89
Andres Garcia Mangas <agmangas@gmail.com>
2018-10-12 12:49:59 +0200

    Use 3 readers instead of 4 and add LED support

--- magician/rfid-lock/rfid-lock.ino
+++ magician/rfid-lock/rfid-lock.ino
@@ -1,5 +1,6 @@
 #include <SerialRFID.h>
 #include <SoftwareSerial.h>
+#include <Adafruit_NeoPixel.h>
 #include "limits.h"
 
 /**
@@ -22,17 +23,37 @@ const byte SECONDARY_RFID_01_RX = 9;
 const byte SECONDARY_RFID_01_TX = 8;
 const byte SECONDARY_RFID_02_RX = 7;
 const byte SECONDARY_RFID_02_TX = 6;
-const byte SECONDARY_RFID_03_RX = 5;
-const byte SECONDARY_RFID_03_TX = 4;
 
-const byte LOCK_RELAY_PIN = 10;
+const byte PIN_RELAY = 10;
 
 /**
    Configuration constants.
 */
 
-const int NUM_READERS = 4;
+const int NUM_READERS = 3;
 const unsigned long MAX_ACTIVATION_DELAY_MS = 3600000;
+
+/**
+   LED constants.
+*/
+
+const int DEFAULT_BRIGHTNESS = 120;
+
+const uint16_t NEOPIXEL_NUMS[NUM_READERS] = {
+  1, 1, 1
+};
+
+const uint8_t NEOPIXEL_PINS[NUM_READERS] = {
+  4, 5, 6
+};
+
+const uint32_t LED_COLORS[NUM_READERS] = {
+  Adafruit_NeoPixel::Color(255, 0, 0),
+  Adafruit_NeoPixel::Color(0, 255, 0),
+  Adafruit_NeoPixel::Color(0, 0, 255)
+};
+
+Adafruit_NeoPixel pixelStrips[NUM_READERS];
 
 /**
    SoftwareSerial instances linked with the RFID sensors.
@@ -40,7 +61,6 @@ const unsigned long MAX_ACTIVATION_DELAY_MS = 3600000;
 
 SoftwareSerial sSerial01(SECONDARY_RFID_01_RX, SECONDARY_RFID_01_TX);
 SoftwareSerial sSerial02(SECONDARY_RFID_02_RX, SECONDARY_RFID_02_TX);
-SoftwareSerial sSerial03(SECONDARY_RFID_03_RX, SECONDARY_RFID_03_TX);
 
 /**
    SerialRFID instances.
@@ -49,7 +69,6 @@ SoftwareSerial sSerial03(SECONDARY_RFID_03_RX, SECONDARY_RFID_03_TX);
 SerialRFID rfidMain(Serial);
 SerialRFID rfid01(sSerial01);
 SerialRFID rfid02(sSerial02);
-SerialRFID rfid03(sSerial03);
 
 /**
    List of accepted tags for each RFID sensor.
@@ -58,8 +77,7 @@ SerialRFID rfid03(sSerial03);
 char acceptedTags[NUM_READERS][SIZE_TAG_ID] = {
   "011005CD9B42",
   "011005CD9C45",
-  "011005CE79A3",
-  "011005CDA079"
+  "011005CE79A3"
 };
 
 /**
@@ -87,17 +105,10 @@ void clearStreamsAndBuffers() {
   clearStream(Serial);
   clearStream(sSerial01);
   clearStream(sSerial02);
-  clearStream(sSerial03);
 }
 
 void updateListenerPort() {
-  if (isTagDefined(0) && isTagDefined(1) && isTagDefined(2)) {
-    if (sSerial03.isListening() == false) {
-      Serial.println("Listening on port #3");
-      Serial.flush();
-      sSerial03.listen();
-    }
-  } else if (isTagDefined(0) && isTagDefined(1)) {
+  if (isTagDefined(0) && isTagDefined(1)) {
     if (sSerial02.isListening() == false) {
       Serial.println("Listening on port #2");
       Serial.flush();
@@ -133,11 +144,9 @@ void readMainTag() {
 
     memset(currentTags[1], 0, sizeof(currentTags[1]));
     memset(currentTags[2], 0, sizeof(currentTags[2]));
-    memset(currentTags[3], 0, sizeof(currentTags[3]));
 
     clearStream(sSerial01);
     clearStream(sSerial02);
-    clearStream(sSerial03);
   }
 }
 
@@ -168,32 +177,39 @@ bool isTagDefined(int portIdx) {
   return strlen(currentTags[portIdx]) == LEN_TAG_ID;
 }
 
-bool tagsAreActivated() {
+bool isTagActivated(int idx) {
   unsigned long now = millis();
 
+  if (strlen(currentTags[idx]) == 0) {
+    // This RFID sensor has not seen any tags yet
+    return false;
+  }
+
+  if (SerialRFID::isEqualTag(currentTags[idx], acceptedTags[idx]) == false) {
+    // Incorrect tag
+    return false;
+  }
+
+  unsigned long diffMs;
+
+  if (currentTagsMillis[idx] > now) {
+    diffMs = (ULONG_MAX - currentTagsMillis[idx]) + now;
+    Serial.println("Clock overflow when checking tag age");
+  } else {
+    diffMs = now - currentTagsMillis[idx];
+  }
+
+  if (diffMs > MAX_ACTIVATION_DELAY_MS) {
+    // The most recent tag is valid but was read too long ago
+    return false;
+  }
+
+  return true;
+}
+
+bool tagsAreActivated() {
   for (int i = 0; i < NUM_READERS; i++) {
-    if (strlen(currentTags[i]) == 0) {
-      // This RFID sensor has not seen any tags yet
-      return false;
-    }
-
-    if (SerialRFID::isEqualTag(currentTags[i], acceptedTags[i]) == false) {
-      // Incorrect tag
-      return false;
-    }
-
-    unsigned long diffMs;
-
-    if (currentTagsMillis[i] > now) {
-      diffMs = (ULONG_MAX - currentTagsMillis[i]) + now;
-      Serial.println("Clock overflow when checking for tag age in ms");
-      Serial.flush();
-    } else {
-      diffMs = now - currentTagsMillis[i];
-    }
-
-    if (diffMs > MAX_ACTIVATION_DELAY_MS) {
-      // The most recent tag is valid but was read too long ago
+    if (isTagActivated(i) == false) {
       return false;
     }
   }
@@ -202,22 +218,77 @@ bool tagsAreActivated() {
 }
 
 /**
+   LED functions.
+*/
+
+void initLedStrips() {
+  for (int i = 0; i < NUM_READERS; i++) {
+    pixelStrips[i] = Adafruit_NeoPixel(NEOPIXEL_NUMS[i], NEOPIXEL_PINS[i], NEO_GRB + NEO_KHZ800);
+    pixelStrips[i].begin();
+    pixelStrips[i].setBrightness(DEFAULT_BRIGHTNESS);
+    pixelStrips[i].clear();
+    pixelStrips[i].show();
+  }
+}
+
+void updateLedStrips() {
+  for (int i = 0; i < NUM_READERS; i++) {
+    if (isTagActivated(i) == true) {
+      for (int j = 0; j < NEOPIXEL_NUMS[i]; j++) {
+        pixelStrips[i].setPixelColor(j, LED_COLORS[i]);
+      }
+    } else {
+      pixelStrips[i].clear();
+    }
+
+    pixelStrips[i].show();
+  }
+}
+
+/**
+   Relay functions
+*/
+
+void lockRelay() {
+  digitalWrite(PIN_RELAY, LOW);
+  progState.relayOpened = false;
+}
+
+void openRelay() {
+  digitalWrite(PIN_RELAY, HIGH);
+  progState.relayOpened = true;
+}
+
+void initRelay() {
+  pinMode(PIN_RELAY, OUTPUT);
+  lockRelay();
+}
+
+void checkStatusToUpdateRelay() {
+  if (tagsAreActivated() == true && progState.relayOpened == false) {
+    Serial.println("All tags have been placed: Opening relay");
+    openRelay();
+  } else if (tagsAreActivated() == false && progState.relayOpened == true) {
+    Serial.println("Invalid tags: Locking relay");
+    lockRelay();
+  }
+}
+
+/**
    Entrypoint.
 */
 
 void setup() {
-  pinMode(LOCK_RELAY_PIN, OUTPUT);
-  digitalWrite(LOCK_RELAY_PIN, LOW);
-
   Serial.begin(9600);
   sSerial01.begin(9600);
   sSerial02.begin(9600);
-  sSerial03.begin(9600);
+
+  lockRelay();
+  updateListenerPort();
+  initLedStrips();
 
   Serial.println(">> Starting RFID electronic lock program");
   Serial.flush();
-
-  updateListenerPort();
 }
 
 void loop() {
@@ -226,14 +297,8 @@ void loop() {
   readMainTag();
   readSecondaryTag(rfid01, 1);
   readSecondaryTag(rfid02, 2);
-  readSecondaryTag(rfid03, 3);
 
-  if (tagsAreActivated() == true && progState.relayOpened == false) {
-    Serial.print("All tags have been placed. Opening lock");
-    Serial.println();
-    Serial.flush();
+  updateLedStrips();
 
-    digitalWrite(LOCK_RELAY_PIN, HIGH);
-    progState.relayOpened = true;
-  }
+  checkStatusToUpdateRelay();
 }
