1a5537c585f1e9209e87b6d3fb34e0aabb5883d9
Andres Garcia Mangas <agmangas@gmail.com>
2018-11-29 20:03:43 +0100

    Take snapshots of LDR bits at regular intervals

--- wizard-school/mandragora/mandragora.ino
+++ wizard-school/mandragora/mandragora.ino
@@ -1,4 +1,5 @@
 #include <Automaton.h>
+#include <CircularBuffer.h>
 
 /**
    LDRs and state machines.
@@ -14,6 +15,20 @@ const int LDR_PULSE_DURATION = 200;
 
 Atm_digital ldrs[LDR_NUM];
 Atm_bit ldrBits[LDR_NUM];
+Atm_timer ldrSnapshotTimer;
+
+/**
+  Buffer of LDR snapshots.
+*/
+
+typedef struct ldrSnapshot {
+  bool states[LDR_NUM];
+} LdrSnapshot;
+
+const int LDR_SNAPSHOT_BUF_SIZE = 50;
+const int LDR_SNAPSHOT_MS = 200;
+
+CircularBuffer<LdrSnapshot, LDR_SNAPSHOT_BUF_SIZE> ldrSnapBuf;
 
 /**
    Audio FX.
@@ -27,8 +42,41 @@ const byte AUDIO_PINS[LDR_NUM] = {
 };
 
 /**
-   LDR functions.
+   State machine functions.
 */
+
+bool equalLdrSnaps(const LdrSnapshot &one, const LdrSnapshot &other) {
+  for (int i = 0; i < LDR_NUM; i++) {
+    if (one.states[i] != other.states[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+void takeLdrSnapshot(int idx, int v, int up) {
+  LdrSnapshot currSnapshot;
+
+  for (int i = 0; i < LDR_NUM; i++) {
+    currSnapshot.states[i] = ldrBits[i].state() == 1;
+  }
+
+  bool shouldPush = ldrSnapBuf.isEmpty() ||
+                    !equalLdrSnaps(currSnapshot, ldrSnapBuf.last());
+
+  if (shouldPush) {
+    Serial.print(F("Pushing LDR snapshot: "));
+
+    for (int i = 0; i < LDR_NUM; i++) {
+      Serial.print(currSnapshot.states[i]);
+    }
+
+    Serial.println();
+
+    ldrSnapBuf.push(currSnapshot);
+  }
+}
 
 void initMachines() {
   for (int i = 0; i < LDR_NUM; i++) {
@@ -40,6 +88,12 @@ void initMachines() {
     .onChange(HIGH, ldrBits[i], ldrBits[i].EVT_ON)
     .onChange(LOW, ldrBits[i], ldrBits[i].EVT_OFF);
   }
+
+  ldrSnapshotTimer
+  .begin(LDR_SNAPSHOT_MS)
+  .repeat(-1)
+  .onTimer(takeLdrSnapshot)
+  .start();
 }
 
 /**
