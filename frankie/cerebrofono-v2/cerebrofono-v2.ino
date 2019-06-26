ea5a40685005251eb56d54f88d95f9d5d7b8a546
Andres Garcia Mangas <agmangas@gmail.com>
2019-06-26 18:49:26 +0200

    Use ID12 reader without breakout

--- frankie/cerebrofono-v2/cerebrofono-v2.ino
+++ frankie/cerebrofono-v2/cerebrofono-v2.ino
@@ -3,8 +3,8 @@
 #include <Adafruit_NeoPixel.h>
 
 /**
-   RFID reader.
-*/
+ * RFID reader.
+ */
 
 const byte PIN_RFID_RX = 2;
 const byte PIN_RFID_TX = 3;
@@ -15,13 +15,8 @@ SerialRFID rfid(sSerial);
 char tag[SIZE_TAG_ID];
 
 /**
-   IDs of valid tags.
-   Track 0: "Cinco de la tarde"
-   Track 1: "Rioturbio"
-   Track 2: "Reina"
-   Track 3: "Cristine"
-   Track 4: "Profesor"
-*/
+ * Valid tag IDs.
+ */
 
 const int NUM_TRACKS = 5;
 const int NUM_IDS_PER_TRACK = 2;
@@ -39,8 +34,8 @@ const char TRACK_TAG_IDS[NUM_TRACKS][NUM_IDS_PER_TRACK][SIZE_TAG_ID] = {
      "1D0028773C7E"}};
 
 /**
-   Audio FX.
-*/
+ * Audio FX.
+ */
 
 const byte PIN_AUDIO_RST = 6;
 const byte PIN_AUDIO_ACT = 7;
@@ -51,19 +46,36 @@ const byte AUDIO_TRACK_PINS[NUM_TRACKS] = {
 const unsigned long AUDIO_TRACK_MAX_MS = 50000;
 
 /**
-   LED strip.
-*/
+ * LED strip.
+ */
 
 const uint16_t NUM_LEDS = 60;
+const uint16_t NUM_LEDS_STATIC = 25;
+
 const uint8_t PIN_LEDS_01 = 4;
 const uint8_t PIN_LEDS_02 = 3;
+const uint8_t PIN_LEDS_STATIC = 5;
+
 const int LED_EFFECT_STEP_MS = 3;
 const int LED_BRIGHTNESS = 150;
 
-Adafruit_NeoPixel pixelStrip01 = Adafruit_NeoPixel(NUM_LEDS, PIN_LEDS_01, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel pixelStrip02 = Adafruit_NeoPixel(NUM_LEDS, PIN_LEDS_02, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixelStrip01 = Adafruit_NeoPixel(
+    NUM_LEDS,
+    PIN_LEDS_01,
+    NEO_GRB + NEO_KHZ800);
+
+Adafruit_NeoPixel pixelStrip02 = Adafruit_NeoPixel(
+    NUM_LEDS,
+    PIN_LEDS_02,
+    NEO_GRB + NEO_KHZ800);
+
+Adafruit_NeoPixel pixelStatic = Adafruit_NeoPixel(
+    NUM_LEDS_STATIC,
+    PIN_LEDS_STATIC,
+    NEO_GRB + NEO_KHZ800);
 
 const uint32_t COLOR_DEFAULT = Adafruit_NeoPixel::Color(126, 32, 198);
+const uint32_t COLOR_STATIC = Adafruit_NeoPixel::Color(200, 200, 200);
 
 const uint32_t AUDIO_TRACK_COLORS[NUM_TRACKS] = {
     Adafruit_NeoPixel::Color(0, 255, 0),
@@ -73,220 +85,237 @@ const uint32_t AUDIO_TRACK_COLORS[NUM_TRACKS] = {
     Adafruit_NeoPixel::Color(255, 255, 255)};
 
 /**
-   Audio FX functions.
-*/
+ * Audio FX functions.
+ */
 
 void playTrack(byte trackPin)
 {
-  if (isTrackPlaying())
-  {
-    Serial.println(F("Skipping: Audio still playing"));
-    return;
-  }
+    if (isTrackPlaying())
+    {
+        Serial.println(F("Skipping: Audio still playing"));
+        return;
+    }
 
-  Serial.print(F("Playing track on pin: "));
-  Serial.println(trackPin);
+    Serial.print(F("Playing track on pin: "));
+    Serial.println(trackPin);
 
-  digitalWrite(trackPin, LOW);
-  pinMode(trackPin, OUTPUT);
-  delay(300);
-  pinMode(trackPin, INPUT);
+    digitalWrite(trackPin, LOW);
+    pinMode(trackPin, OUTPUT);
+    delay(200);
+    pinMode(trackPin, INPUT);
 }
 
 void initAudioPins()
 {
-  for (int i = 0; i < NUM_TRACKS; i++)
-  {
-    pinMode(AUDIO_TRACK_PINS[i], INPUT);
-  }
+    for (int i = 0; i < NUM_TRACKS; i++)
+    {
+        pinMode(AUDIO_TRACK_PINS[i], INPUT);
+    }
 
-  pinMode(PIN_AUDIO_ACT, INPUT);
-  pinMode(PIN_AUDIO_RST, INPUT);
+    pinMode(PIN_AUDIO_ACT, INPUT);
+    pinMode(PIN_AUDIO_RST, INPUT);
 }
 
 bool isTrackPlaying()
 {
-  return digitalRead(PIN_AUDIO_ACT) == LOW;
+    return digitalRead(PIN_AUDIO_ACT) == LOW;
 }
 
 void resetAudio()
 {
-  Serial.println(F("Audio FX reset"));
+    Serial.println(F("Audio FX reset"));
 
-  digitalWrite(PIN_AUDIO_RST, LOW);
-  pinMode(PIN_AUDIO_RST, OUTPUT);
-  delay(10);
-  pinMode(PIN_AUDIO_RST, INPUT);
+    digitalWrite(PIN_AUDIO_RST, LOW);
+    pinMode(PIN_AUDIO_RST, OUTPUT);
+    delay(100);
+    pinMode(PIN_AUDIO_RST, INPUT);
 
-  Serial.println(F("Waiting for Audio FX startup"));
+    Serial.println(F("Waiting for Audio FX startup"));
 
-  delay(2000);
+    delay(2000);
 }
 
 /**
-   LED strip functions.
-*/
+ * LED strip functions.
+ */
 
 void displayAudioLedEffect(int tagIdx)
 {
-  int limitLo = floor(NUM_LEDS * 0.45);
-  int limitHi = floor(NUM_LEDS * 1.00);
+    int limitLo = floor(NUM_LEDS * 0.45);
+    int limitHi = floor(NUM_LEDS * 1.00);
 
-  uint32_t trackColor = AUDIO_TRACK_COLORS[tagIdx];
+    uint32_t trackColor = AUDIO_TRACK_COLORS[tagIdx];
 
-  unsigned long now;
-  unsigned long ini = millis();
-  bool timeout = false;
+    unsigned long now;
+    unsigned long ini = millis();
+    bool timeout = false;
 
-  int currTarget;
-  int currLevel;
+    int currTarget;
+    int currLevel;
 
-  while (isTrackPlaying() && !timeout)
-  {
-    currTarget = random(limitLo, limitHi);
-    currLevel = 0;
+    while (isTrackPlaying() && !timeout)
+    {
+        currTarget = random(limitLo, limitHi);
+        currLevel = 0;
+
+        clearLeds();
+
+        for (int i = 0; i < currTarget; i++)
+        {
+            pixelStrip01.setPixelColor(i, trackColor);
+            pixelStrip01.show();
+            pixelStrip02.setPixelColor(i, trackColor);
+            pixelStrip02.show();
+            delay(LED_EFFECT_STEP_MS);
+        }
+
+        for (int i = (currTarget - 1); i >= 0; i--)
+        {
+            pixelStrip01.setPixelColor(i, 0);
+            pixelStrip01.show();
+            pixelStrip02.setPixelColor(i, 0);
+            pixelStrip02.show();
+            delay(LED_EFFECT_STEP_MS);
+        }
+
+        now = millis();
+
+        if (now < ini)
+        {
+            Serial.println(F("Audio timeout: clock overflow"));
+            timeout = true;
+        }
+        else if ((now - ini) > AUDIO_TRACK_MAX_MS)
+        {
+            Serial.println(F("Audio timeout"));
+            timeout = true;
+        }
+    }
 
     clearLeds();
-
-    for (int i = 0; i < currTarget; i++)
-    {
-      pixelStrip01.setPixelColor(i, trackColor);
-      pixelStrip01.show();
-      pixelStrip02.setPixelColor(i, trackColor);
-      pixelStrip02.show();
-      delay(LED_EFFECT_STEP_MS);
-    }
-
-    for (int i = (currTarget - 1); i >= 0; i--)
-    {
-      pixelStrip01.setPixelColor(i, 0);
-      pixelStrip01.show();
-      pixelStrip02.setPixelColor(i, 0);
-      pixelStrip02.show();
-      delay(LED_EFFECT_STEP_MS);
-    }
-
-    now = millis();
-
-    if (now < ini)
-    {
-      Serial.println(F("Audio timeout: clock overflow"));
-      timeout = true;
-    }
-    else if ((now - ini) > AUDIO_TRACK_MAX_MS)
-    {
-      Serial.println(F("Audio timeout"));
-      timeout = true;
-    }
-  }
-
-  clearLeds();
 }
 
 void initLeds()
 {
-  pixelStrip01.begin();
-  pixelStrip01.setBrightness(LED_BRIGHTNESS);
-  pixelStrip01.show();
+    pixelStrip01.begin();
+    pixelStrip01.setBrightness(LED_BRIGHTNESS);
+    pixelStrip01.show();
 
-  pixelStrip02.begin();
-  pixelStrip02.setBrightness(LED_BRIGHTNESS);
-  pixelStrip02.show();
+    pixelStrip02.begin();
+    pixelStrip02.setBrightness(LED_BRIGHTNESS);
+    pixelStrip02.show();
 
-  clearLeds();
+    pixelStatic.begin();
+    pixelStatic.setBrightness(LED_BRIGHTNESS);
+    pixelStatic.show();
+
+    clearLeds();
 }
 
 void clearLeds()
 {
-  pixelStrip01.clear();
-  pixelStrip01.show();
+    pixelStrip01.clear();
+    pixelStrip01.show();
 
-  pixelStrip02.clear();
-  pixelStrip02.show();
+    pixelStrip02.clear();
+    pixelStrip02.show();
 }
 
 void playLedStartupPattern()
 {
-  clearLeds();
+    clearLeds();
 
-  const int delayMs = 5;
-  const uint32_t color = Adafruit_NeoPixel::Color(200, 200, 200);
+    const int delayMs = 5;
+    const uint32_t color = Adafruit_NeoPixel::Color(200, 200, 200);
 
-  for (int i = 0; i < NUM_LEDS; i++)
-  {
-    pixelStrip01.setPixelColor(i, color);
-    pixelStrip01.show();
-    delay(delayMs);
-  }
+    for (int i = 0; i < NUM_LEDS; i++)
+    {
+        pixelStrip01.setPixelColor(i, color);
+        pixelStrip01.show();
+        delay(delayMs);
+    }
 
-  clearLeds();
+    clearLeds();
+}
+
+void showStaticLed()
+{
+    pixelStatic.clear();
+
+    for (int i = 0; i < NUM_LEDS_STATIC; i++)
+    {
+        pixelStatic.setPixelColor(i, COLOR_STATIC);
+    }
+
+    pixelStatic.show();
 }
 
 /**
-   RFID reader functions.
-*/
+ * RFID reader functions.
+ */
 
 int readCurrentTagIndex()
 {
-  bool tagFound = rfid.readTag(tag, sizeof(tag));
+    bool tagFound = rfid.readTag(tag, sizeof(tag));
 
-  if (!tagFound)
-  {
-    return -1;
-  }
-
-  Serial.print(F("Tag: "));
-  Serial.println(tag);
-
-  for (int i = 0; i < NUM_TRACKS; i++)
-  {
-    for (int j = 0; j < NUM_IDS_PER_TRACK; j++)
+    if (!tagFound)
     {
-      if (SerialRFID::isEqualTag(tag, TRACK_TAG_IDS[i][j]))
-      {
-        Serial.print(F("Track match: "));
-        Serial.println(i);
-        return i;
-      }
+        return -1;
     }
-  }
 
-  return -1;
+    Serial.print(F("Tag: "));
+    Serial.println(tag);
+
+    for (int i = 0; i < NUM_TRACKS; i++)
+    {
+        for (int j = 0; j < NUM_IDS_PER_TRACK; j++)
+        {
+            if (SerialRFID::isEqualTag(tag, TRACK_TAG_IDS[i][j]))
+            {
+                Serial.print(F("Track match: "));
+                Serial.println(i);
+                return i;
+            }
+        }
+    }
+
+    return -1;
 }
 
 void readTagAndPlayAudio()
 {
-  int tagIdx = readCurrentTagIndex();
+    int tagIdx = readCurrentTagIndex();
 
-  if (tagIdx == -1)
-  {
-    return;
-  }
+    if (tagIdx == -1)
+    {
+        return;
+    }
 
-  playTrack(AUDIO_TRACK_PINS[tagIdx]);
-  displayAudioLedEffect(tagIdx);
+    playTrack(AUDIO_TRACK_PINS[tagIdx]);
+    displayAudioLedEffect(tagIdx);
 }
 
 /**
-   Entrypoint.
-*/
+ * Entrypoint.
+ */
 
 void setup()
 {
-  Serial.begin(9600);
-  sSerial.begin(9600);
+    Serial.begin(9600);
+    sSerial.begin(9600);
 
-  initAudioPins();
-  resetAudio();
-  initLeds();
-  playLedStartupPattern();
-  sSerial.listen();
+    initAudioPins();
+    resetAudio();
+    initLeds();
+    playLedStartupPattern();
+    sSerial.listen();
+    showStaticLed();
 
-  Serial.println(F(">> Starting Cerebrofono program"));
+    Serial.println(F(">> Starting Cerebrofono program"));
 }
 
 void loop()
 {
-  readTagAndPlayAudio();
-  delay(100);
+    readTagAndPlayAudio();
+    delay(100);
 }
