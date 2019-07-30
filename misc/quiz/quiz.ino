3185beb77a95c7801eb77b4e40780ead1e24f9e1
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2019-07-30 17:49:20 +0200

    First draft of the quiz program

--- misc/quiz/quiz.ino
+++ misc/quiz/quiz.ino
@@ -22,6 +22,13 @@ Atm_button buttons[PLAYERS_NUM][OPTIONS_NUM];
 Atm_led buttonsLeds[PLAYERS_NUM][OPTIONS_NUM];
 
 /**
+ * Show host button.
+ */
+
+const int HOST_BUTTON_PIN = A6;
+Atm_button buttonHost;
+
+/**
  * Countdown timer.
  */
 
@@ -30,32 +37,25 @@ Atm_timer timerCountdown;
 const int TIMER_COUNTDOWN_MS = 5000;
 
 /**
- * Program state.
+ * Countdown LED timer.
  */
 
-int currChoices[PLAYERS_NUM];
+Atm_timer timerLedCountdown;
 
-typedef struct programState
-{
-    int *currChoices;
-} ProgramState;
+const int TIMER_LED_COUNTDOWN_MS = 150;
 
-ProgramState progState = {
-    .currChoices = currChoices};
+/**
+ * Colors.
+ */
 
-void initState()
-{
-    for (int p = 0; p < PLAYERS_NUM; p++)
-    {
-        progState.currChoices[p] = 0;
-    }
-}
+const uint32_t COLOR_CORRECT = Adafruit_NeoPixel::Color(0, 255, 0);
+const uint32_t COLOR_ERROR = Adafruit_NeoPixel::Color(255, 0, 0);
 
 /**
  * Player LED strips.
  */
 
-const int LED_PLAYER_BRIGHTNESS = 200;
+const int LED_PLAYER_BRIGHTNESS = 150;
 
 const int LED_PLAYER_PINS[PLAYERS_NUM] = {
     8, 9, 10};
@@ -78,8 +78,95 @@ Adafruit_NeoPixel ledPlayerStrips[PLAYERS_NUM] = {
         NEO_RGB + NEO_KHZ800)};
 
 /**
+ * Countdown LED strip.
+ */
+
+const int LED_COUNTDOWN_BRIGHTNESS = 220;
+const int LED_COUNTDOWN_PIN = 11;
+const int LED_COUNTDOWN_NUM = 10;
+
+Adafruit_NeoPixel ledCountdown = Adafruit_NeoPixel(
+    LED_COUNTDOWN_NUM,
+    LED_COUNTDOWN_PIN,
+    NEO_RGB + NEO_KHZ800);
+
+/**
+ * Global scoreboard strip.
+ */
+
+const int LED_GLOBAL_BRIGHTNESS = 220;
+const int LED_GLOBAL_PIN = 12;
+const int LED_GLOBAL_NUM = 30;
+
+Adafruit_NeoPixel ledGlobal = Adafruit_NeoPixel(
+    LED_GLOBAL_NUM,
+    LED_GLOBAL_PIN,
+    NEO_RGB + NEO_KHZ800);
+
+/**
+ * Solution key.
+ */
+
+const int NUM_PHASES = 10;
+
+const int SOLUTION_KEY[NUM_PHASES] = {
+    0, 1, 0, 1, 0, 1, 0, 1, 0, 1};
+
+/**
+ * Program state.
+ */
+
+typedef struct programState
+{
+    int currChoices[PLAYERS_NUM];
+    bool phaseResults[PLAYERS_NUM][NUM_PHASES];
+    int currPhase;
+    unsigned long countdownStartMillis;
+} ProgramState;
+
+ProgramState progState = {
+    .currChoices = {0},
+    .phaseResults = {{false}},
+    .currPhase = 0,
+    .countdownStartMillis = 0};
+
+void initState()
+{
+    for (int p = 0; p < PLAYERS_NUM; p++)
+    {
+        for (int f = 0; f < NUM_PHASES; f++)
+        {
+            progState.phaseResults[p][f] = false;
+        }
+
+        progState.currChoices[p] = 0;
+    }
+}
+
+/**
  * Player LED functions.
  */
+
+void showPlayerLedCorrect(int pIdx)
+{
+    ledPlayerStrips[pIdx].fill(COLOR_CORRECT);
+    ledPlayerStrips[pIdx].show();
+}
+
+void showPlayerLedError(int pIdx)
+{
+    ledPlayerStrips[pIdx].fill(COLOR_ERROR);
+    ledPlayerStrips[pIdx].show();
+}
+
+void clearPlayerLeds()
+{
+    for (int p = 0; p < PLAYERS_NUM; p++)
+    {
+        ledPlayerStrips[p].clear();
+        ledPlayerStrips[p].show();
+    }
+}
 
 void initPlayerLeds()
 {
@@ -93,6 +180,74 @@ void initPlayerLeds()
 }
 
 /**
+ * Countdown LED functions.
+ */
+
+void initCountdownLed()
+{
+    ledCountdown.begin();
+    ledCountdown.setBrightness(LED_COUNTDOWN_BRIGHTNESS);
+    ledCountdown.clear();
+    ledCountdown.show();
+}
+
+/**
+ * Global scoreboard LED strip functions.
+ */
+
+void showGlobalLed()
+{
+    ledGlobal.clear();
+
+    int ledsPerPlayer = floor(((float)LED_GLOBAL_NUM) / ((float)PLAYERS_NUM));
+    int ledsPerPhase = floor(((float)ledsPerPlayer) / ((float)NUM_PHASES));
+
+    if (ledsPerPhase <= 0)
+    {
+        Serial.println(F("Warning :: Invalid number of global LEDs"));
+        return;
+    }
+
+    int playerOffset;
+    int phaseOffset;
+    int currLedIdx;
+
+    for (int p = 0; p < PLAYERS_NUM; p++)
+    {
+        playerOffset = p * ledsPerPlayer;
+
+        for (int f = 0; f < progState.currPhase; f++)
+        {
+            phaseOffset = f * ledsPerPhase;
+
+            for (int i = 0; i < ledsPerPhase; i++)
+            {
+                currLedIdx = playerOffset + phaseOffset + i;
+
+                if (currLedIdx >= (playerOffset + ledsPerPlayer))
+                {
+                    Serial.println(F("Warning :: Global LED overflow"));
+                }
+
+                ledGlobal.setPixelColor(
+                    currLedIdx,
+                    progState.phaseResults[p][f] ? COLOR_CORRECT : COLOR_ERROR);
+            }
+        }
+    }
+
+    ledGlobal.show();
+}
+
+void initGlobalLed()
+{
+    ledGlobal.begin();
+    ledGlobal.setBrightness(LED_GLOBAL_BRIGHTNESS);
+    ledGlobal.clear();
+    ledGlobal.show();
+}
+
+/**
  * Countdown timer functions.
  */
 
@@ -101,9 +256,58 @@ bool isTimerCountdownIdle()
     return timerCountdown.state() == Atm_timer::IDLE;
 }
 
+void blockOnFinish()
+{
+    while (true)
+    {
+        Serial.println(F("The end"));
+        delay(2000);
+    }
+}
+
 void onTimerCountdown(int idx, int v, int up)
 {
-    Serial.println(F("Countdown timer"));
+    Serial.println(F("Countdown timer finished"));
+
+    if (progState.currPhase >= NUM_PHASES)
+    {
+        Serial.println(F("Ignoring countdown timer :: Final phase"));
+        return;
+    }
+
+    int validChoice = SOLUTION_KEY[progState.currPhase];
+    bool isValidChoice;
+
+    for (int p = 0; p < PLAYERS_NUM; p++)
+    {
+        isValidChoice = progState.currChoices[p] == validChoice;
+        progState.phaseResults[p][progState.currPhase] = isValidChoice;
+
+        Serial.print(F("Player #"));
+        Serial.print(p);
+        Serial.print(F(" :: Phase #"));
+        Serial.print(progState.currPhase);
+
+        if (isValidChoice)
+        {
+            Serial.println(F(" :: OK"));
+            showPlayerLedCorrect(p);
+        }
+        else
+        {
+            Serial.println(F(" :: Error"));
+            showPlayerLedError(p);
+        }
+    }
+
+    progState.currPhase++;
+    progState.countdownStartMillis = 0;
+    showGlobalLed();
+
+    if (progState.currPhase >= NUM_PHASES)
+    {
+        blockOnFinish();
+    }
 }
 
 void initTimerCountdown()
@@ -119,12 +323,58 @@ void startTimerCountdown()
     if (isTimerCountdownIdle())
     {
         Serial.println(F("Starting countdown timer"));
+        clearPlayerLeds();
+        progState.countdownStartMillis = millis();
         timerCountdown.start();
     }
     else
     {
         Serial.println(F("Countdown timer is not idle"));
     }
+}
+
+/**
+ * LEDs timer functions.
+ */
+
+void onTimerLedCountdown(int idx, int v, int up)
+{
+    ledCountdown.clear();
+
+    if (progState.countdownStartMillis == 0)
+    {
+        ledCountdown.show();
+        return;
+    }
+
+    unsigned long now = millis();
+    unsigned long endMillis = progState.countdownStartMillis + TIMER_COUNTDOWN_MS;
+
+    if (now >= endMillis)
+    {
+        ledCountdown.show();
+        return;
+    }
+
+    unsigned long diff = endMillis - now;
+    float ratio = ((float)diff) / ((float)TIMER_COUNTDOWN_MS);
+    int numLedsOn = floor(LED_COUNTDOWN_NUM * ratio);
+
+    for (int i = 0; i < numLedsOn; i++)
+    {
+        ledCountdown.setPixelColor(i, COLOR_CORRECT);
+    }
+
+    ledCountdown.show();
+}
+
+void initTimerLedCountdown()
+{
+    timerLedCountdown
+        .begin(TIMER_LED_COUNTDOWN_MS)
+        .repeat(-1)
+        .onTimer(onTimerLedCountdown)
+        .start();
 }
 
 /**
@@ -165,7 +415,7 @@ void onPlayerButton(int idx, int v, int up)
     progState.currChoices[plyIdx] = optIdx;
 }
 
-void initButtons()
+void initPlayerButtons()
 {
     for (int p = 0; p < PLAYERS_NUM; p++)
     {
@@ -183,6 +433,23 @@ void initButtons()
 }
 
 /**
+ * Show host button functions.
+ */
+
+void onHostButton(int idx, int v, int up)
+{
+    Serial.println(F("Host button"));
+    startTimerCountdown();
+}
+
+void initHostButton()
+{
+    buttonHost
+        .begin(HOST_BUTTON_PIN)
+        .onPress(onHostButton);
+}
+
+/**
  * Entrypoint.
  */
 
@@ -191,9 +458,13 @@ void setup()
     Serial.begin(9600);
 
     initState();
-    initButtons();
+    initPlayerButtons();
+    initHostButton();
     initPlayerLeds();
     initTimerCountdown();
+    initCountdownLed();
+    initGlobalLed();
+    initTimerLedCountdown();
 
     Serial.println(F(">> Starting quiz program"));
 }
