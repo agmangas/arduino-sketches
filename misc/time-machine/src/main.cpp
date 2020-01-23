0c43a03e3b8b13ef3eac79036852bffb81ba8600
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2020-01-23 18:00:56 +0100

    Move from Nano to Feather M4

--- misc/time-machine/src/main.cpp
+++ misc/time-machine/src/main.cpp
@@ -1,26 +1,99 @@
 #include <Adafruit_NeoPixel.h>
+#include <Adafruit_VS1053.h>
 #include <Automaton.h>
+#include <SD.h>
+#include <SPI.h>
+
+#define VS1053_RESET -1 // VS1053 reset pin (not used)
+#define VS1053_CS 6 // VS1053 chip select pin (output)
+#define VS1053_DCS 10 // VS1053 Data/command select pin (output)
+#define CARDCS 5 // Card chip select pin
+#define VS1053_DREQ 9 // VS1053 Data request, ideally an Interrupt pin
+
+/**
+ * Audio player instance.
+ */
+
+Adafruit_VS1053_FilePlayer musicPlayer = Adafruit_VS1053_FilePlayer(
+    VS1053_RESET,
+    VS1053_CS,
+    VS1053_DCS,
+    VS1053_DREQ,
+    CARDCS);
+
+/**
+ * Audio track names. 
+ */
+
+const String TRACK_LED_EFFECT = String("/effect.mp3");
+
+/**
+ * Number of wheel bars.
+ */
 
 const uint8_t BARS_NUM = 4;
-const uint8_t LED_BRIGHTNESS = 200;
+
+/**
+ * Default LED brightness
+ */
+
+const uint8_t LED_BRIGHTNESS = 180;
+
+/**
+ * Wheel bar LED strips.
+ */
+
 const uint16_t LED_NUM_BARS = 20;
-const uint16_t LED_NUM_WHEEL = 120;
+const uint8_t LED_NUM_PINS[BARS_NUM] = { A2, A3, A4, A5 };
 
 Adafruit_NeoPixel ledsBar[BARS_NUM] = {
-    Adafruit_NeoPixel(LED_NUM_BARS, 2, NEO_GRB + NEO_KHZ800),
-    Adafruit_NeoPixel(LED_NUM_BARS, 3, NEO_GRB + NEO_KHZ800),
-    Adafruit_NeoPixel(LED_NUM_BARS, 4, NEO_GRB + NEO_KHZ800),
-    Adafruit_NeoPixel(LED_NUM_BARS, 5, NEO_GRB + NEO_KHZ800)
+    Adafruit_NeoPixel(LED_NUM_BARS, LED_NUM_PINS[0], NEO_GRB + NEO_KHZ800),
+    Adafruit_NeoPixel(LED_NUM_BARS, LED_NUM_PINS[1], NEO_GRB + NEO_KHZ800),
+    Adafruit_NeoPixel(LED_NUM_BARS, LED_NUM_PINS[2], NEO_GRB + NEO_KHZ800),
+    Adafruit_NeoPixel(LED_NUM_BARS, LED_NUM_PINS[3], NEO_GRB + NEO_KHZ800)
 };
 
+/**
+ * Wheel LED strip.
+ */
+
+const uint16_t LED_NUM_WHEEL = 120;
+const uint8_t LED_PIN_WHEEL = 4;
+
 Adafruit_NeoPixel ledWheel = Adafruit_NeoPixel(
-    LED_NUM_WHEEL, 6, NEO_GRB + NEO_KHZ800);
+    LED_NUM_WHEEL,
+    LED_PIN_WHEEL,
+    NEO_GRB + NEO_KHZ800);
+
+/**
+ * LED effects timer.
+ */
 
 Atm_timer timerLed;
 
 const uint16_t TIMER_LED_MS = 20;
 const uint16_t TIMER_MODULO_BARS = 2;
 const uint16_t TIMER_MODULO_WHEEL = 1;
+
+/**
+ * LED unlock button.
+ */
+
+const uint8_t BUTTON_UNLOCK_PIN = 11;
+
+Atm_button buttonUnlock;
+
+/**
+ * Built-in Neopixel.
+ */
+
+const uint16_t LED_NUM_BOARD = 1;
+const uint8_t LED_PIN_BOARD = 8;
+
+Adafruit_NeoPixel boardPixel = Adafruit_NeoPixel(
+    LED_NUM_BOARD,
+    LED_PIN_BOARD,
+    NEO_GRB + NEO_KHZ800);
 
 typedef struct programState {
     bool isLedBarsUnlocked;
@@ -43,6 +116,79 @@ void initState()
     progState.ledPivotWheel = 0;
 }
 
+void printDirectory(File dir, int numTabs)
+{
+    while (true) {
+        File entry = dir.openNextFile();
+
+        if (!entry) {
+            // No more files
+            break;
+        }
+
+        for (uint8_t i = 0; i < numTabs; i++) {
+            Serial.print('\t');
+        }
+
+        Serial.print(entry.name());
+
+        if (entry.isDirectory()) {
+            Serial.println("/");
+            printDirectory(entry, numTabs + 1);
+        } else {
+            // Files have sizes, directories do not
+            Serial.print("\t\t");
+            Serial.println(entry.size(), DEC);
+        }
+
+        entry.close();
+    }
+}
+
+void initAudio()
+{
+    const uint8_t volLeft = 0;
+    const uint8_t volRight = 0;
+
+    if (!musicPlayer.begin()) {
+        Serial.println(F("VS1053 not found"));
+        while (true) {
+            delay(1);
+        }
+    }
+
+    Serial.println(F("VS1053 OK"));
+
+    if (!SD.begin(CARDCS)) {
+        Serial.println(F("SD failer or not found"));
+        while (true) {
+            delay(1);
+        }
+    }
+
+    Serial.println(F("SD OK"));
+    Serial.println(F("Listing SD root"));
+
+    printDirectory(SD.open("/"), 0);
+
+    // Set volume for left, right channels
+    // lower numbers == louder volume
+    musicPlayer.setVolume(volLeft, volRight);
+    musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);
+}
+
+void playTrack(String trackName)
+{
+    if (!musicPlayer.stopped()) {
+        Serial.println(F("Cannot play track: musicPlayer.stopped() != false"));
+    }
+
+    Serial.print(F("Playing: "));
+    Serial.println(trackName);
+
+    musicPlayer.startPlayingFile(trackName.c_str());
+}
+
 void initLeds()
 {
     for (int i = 0; i < BARS_NUM; i++) {
@@ -56,6 +202,11 @@ void initLeds()
     ledWheel.setBrightness(LED_BRIGHTNESS);
     ledWheel.clear();
     ledWheel.show();
+
+    boardPixel.begin();
+    boardPixel.setBrightness(LED_BRIGHTNESS);
+    boardPixel.clear();
+    boardPixel.show();
 }
 
 void ledTick()
@@ -122,10 +273,31 @@ void refreshLedWheel()
     ledWheel.show();
 }
 
+void updateUnlockState()
+{
+    if (!progState.isLedBarsUnlocked || progState.isLedWheelUnlocked) {
+        return;
+    }
+
+    uint16_t numPixelsBars = 0;
+
+    for (uint8_t i = 0; i < LED_NUM_BARS; i++) {
+        numPixelsBars = ledsBar[i].numPixels() > numPixelsBars
+            ? ledsBar[i].numPixels()
+            : numPixelsBars;
+    }
+
+    if (progState.ledPivotBars >= numPixelsBars) {
+        Serial.println(F("Unlocking wheel LED"));
+        progState.isLedWheelUnlocked = true;
+    }
+}
+
 void onLedTimer(int idx, int v, int up)
 {
     refreshLedBars();
     refreshLedWheel();
+    updateUnlockState();
     ledTick();
 }
 
@@ -138,6 +310,20 @@ void initLedTimer()
         .start();
 }
 
+void onPressUnlock(int idx, int v, int up)
+{
+    Serial.println(F("Unlocking bars LED"));
+    progState.isLedBarsUnlocked = true;
+    playTrack(TRACK_LED_EFFECT);
+}
+
+void initButtons()
+{
+    buttonUnlock
+        .begin(BUTTON_UNLOCK_PIN)
+        .onPress(onPressUnlock);
+}
+
 void setup()
 {
     Serial.begin(9600);
@@ -145,8 +331,13 @@ void setup()
     initState();
     initLeds();
     initLedTimer();
+    initButtons();
+    initAudio();
 
     Serial.println(F(">> Time machine"));
+
+    boardPixel.fill(Adafruit_NeoPixel::Color(0, 255, 0));
+    boardPixel.show();
 }
 
 void loop()
