5f2b490d07225955a5ac1c239b7e4a61b4a2e034
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2020-01-31 12:29:59 +0100

    Add multiple valid options for each reader

--- magician/frames-lock/frames-lock.ino
+++ magician/frames-lock/frames-lock.ino
@@ -20,21 +20,23 @@ RDM6300 rfid03(6, 7);
 RDM6300 rfid04(8, 9);
 
 RDM6300 rfidReaders[NUM_READERS] = {
-  rfid01,
-  rfid02,
-  rfid03,
-  rfid04
+    rfid01,
+    rfid02,
+    rfid03,
+    rfid04
 };
 
 const unsigned int EMPTY_TOLERANCE = 1;
 
 String currentTags[NUM_READERS];
 
-String validTags[NUM_READERS] = {
-  "1D00278F8200",
-  "1D00277EC900",
-  "1D00278E7600",
-  "1D0027AA9B00"
+const int NUM_OPTIONS = 2;
+
+String validTags[NUM_READERS][NUM_OPTIONS] = {
+    { "1D00278F8200", "1D00278F8200" },
+    { "1D00277EC900", "1D00277EC900" },
+    { "1D00278E7600", "1D00278E7600" },
+    { "1D0027AA9B00", "1D0027AA9B00" }
 };
 
 /**
@@ -42,123 +44,142 @@ String validTags[NUM_READERS] = {
 */
 
 typedef struct programState {
-  bool relayOpened;
-  unsigned int emptyReadCount[NUM_READERS];
+    bool relayOpened;
+    unsigned int emptyReadCount[NUM_READERS];
 } ProgramState;
 
 ProgramState progState = {
-  .relayOpened = false,
-  .emptyReadCount = {0, 0, 0, 0}
+    .relayOpened = false,
+    .emptyReadCount = { 0, 0, 0, 0 }
 };
 
 /**
    RFID modules functions
 */
 
-void initRfidReaders() {
-  for (int i = 0; i < NUM_READERS; i++) {
-    rfidReaders[i].begin();
-  }
+void initRfidReaders()
+{
+    for (int i = 0; i < NUM_READERS; i++) {
+        rfidReaders[i].begin();
+    }
 }
 
-void pollRfidReaders() {
-  String tagId;
+void pollRfidReaders()
+{
+    String tagId;
 
-  for (int i = 0; i < NUM_READERS; i++) {
-    tagId = rfidReaders[i].getTagId();
+    for (int i = 0; i < NUM_READERS; i++) {
+        tagId = rfidReaders[i].getTagId();
 
-    if (tagId.length()) {
-      progState.emptyReadCount[i] = 0;
-    } else if (progState.emptyReadCount[i] <= EMPTY_TOLERANCE) {
-      progState.emptyReadCount[i] += 1;
+        if (tagId.length()) {
+            progState.emptyReadCount[i] = 0;
+        } else if (progState.emptyReadCount[i] <= EMPTY_TOLERANCE) {
+            progState.emptyReadCount[i] += 1;
+        }
+
+        if (!tagId.length() && currentTags[i].length() && progState.emptyReadCount[i] <= EMPTY_TOLERANCE) {
+            Serial.print("Ignoring empty read on reader: ");
+            Serial.println(i);
+            return;
+        }
+
+        currentTags[i] = tagId;
     }
-
-    if (!tagId.length() &&
-        currentTags[i].length() &&
-        progState.emptyReadCount[i] <= EMPTY_TOLERANCE) {
-      Serial.print("Ignoring empty read on reader: ");
-      Serial.println(i);
-      return;
-    }
-
-    currentTags[i] = tagId;
-  }
 }
 
-bool isTagDefined(int idx) {
-  return currentTags[idx].length() > 0;
+bool isTagDefined(int idx)
+{
+    return currentTags[idx].length() > 0;
 }
 
-bool areCurrentTagsValid() {
-  for (int i = 0; i < NUM_READERS; i++) {
-    if (!isTagDefined(i)) {
-      return false;
+bool areCurrentTagsValid()
+{
+    bool isValid;
+
+    for (int i = 0; i < NUM_READERS; i++) {
+        if (!isTagDefined(i)) {
+            return false;
+        }
+
+        isValid = false;
+
+        for (int j = 0; j < NUM_OPTIONS; j++) {
+            if (validTags[i][j].compareTo(currentTags[i]) == 0) {
+                isValid = true;
+                break;
+            }
+        }
+
+        if (!isValid) {
+            return false;
+        }
     }
 
-    if (validTags[i].compareTo(currentTags[i]) != 0) {
-      return false;
-    }
-  }
-
-  return true;
+    return true;
 }
 
-void printCurrentTags() {
-  Serial.print("## Current tags :: ");
-  Serial.println(millis());
+void printCurrentTags()
+{
+    Serial.print("## Current tags :: ");
+    Serial.println(millis());
 
-  for (int i = 0; i < NUM_READERS; i++) {
-    Serial.print(i);
-    Serial.print(" :: ");
-    Serial.println(currentTags[i]);
-  }
+    for (int i = 0; i < NUM_READERS; i++) {
+        Serial.print(i);
+        Serial.print(" :: ");
+        Serial.println(currentTags[i]);
+    }
 }
 
 /**
    Relay functions
 */
 
-void lockRelay() {
-  digitalWrite(PIN_RELAY, LOW);
-  progState.relayOpened = false;
+void lockRelay()
+{
+    digitalWrite(PIN_RELAY, LOW);
+    progState.relayOpened = false;
 }
 
-void openRelay() {
-  digitalWrite(PIN_RELAY, HIGH);
-  progState.relayOpened = true;
+void openRelay()
+{
+    digitalWrite(PIN_RELAY, HIGH);
+    progState.relayOpened = true;
 }
 
-void initRelay() {
-  pinMode(PIN_RELAY, OUTPUT);
-  lockRelay();
-}
-
-void checkStatusToUpdateRelay() {
-  if (areCurrentTagsValid() == true && progState.relayOpened == false) {
-    Serial.println("Opening relay");
-    openRelay();
-  } else if (areCurrentTagsValid() == false && progState.relayOpened == true) {
-    Serial.println("Locking relay");
+void initRelay()
+{
+    pinMode(PIN_RELAY, OUTPUT);
     lockRelay();
-  }
+}
+
+void checkStatusToUpdateRelay()
+{
+    if (areCurrentTagsValid() == true && progState.relayOpened == false) {
+        Serial.println("Opening relay");
+        openRelay();
+    } else if (areCurrentTagsValid() == false && progState.relayOpened == true) {
+        Serial.println("Locking relay");
+        lockRelay();
+    }
 }
 
 /**
    Entrypoint
 */
 
-void setup() {
-  Serial.begin(9600);
+void setup()
+{
+    Serial.begin(9600);
 
-  initRfidReaders();
-  initRelay();
+    initRfidReaders();
+    initRelay();
 
-  Serial.println(">> Starting frames lock program");
+    Serial.println(">> Starting frames lock program");
 }
 
-void loop() {
-  pollRfidReaders();
-  printCurrentTags();
-  checkStatusToUpdateRelay();
+void loop()
+{
+    pollRfidReaders();
+    printCurrentTags();
+    checkStatusToUpdateRelay();
 }
-
